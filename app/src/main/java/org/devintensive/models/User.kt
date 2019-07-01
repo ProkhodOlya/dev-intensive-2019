81fbcf13bc0a2a7cbf5790de5ee4aa425130aaf6
ProkhodOlya <prohoda_o@mail.ru>
2019-07-01 22:29:08 +0300

    Add Builder

--- app/src/main/java/org/devintensive/models/User.kt
+++ app/src/main/java/org/devintensive/models/User.kt
@@ -13,8 +13,38 @@ data class User(
     var lastVisit: Date? = Date(),
     var isOnline: Boolean = false
 ) {
+    data class Builder(
+        val id: String,
+        var firstName: String?,
+        var lastName: String?,
+        var avatar: String?,
+        var rating: Int = 0,
+        var respect: Int = 0,
+        var lastVisit: Date? = Date(),
+        var isOnline: Boolean = false
+    ) {
+        fun firstName(firstName: String) = apply { this.firstName = firstName }
+        fun lastName(lastName: String) = apply { this.lastName = lastName }
+        fun avatar(avatar: String) = apply { this.avatar = avatar }
+        fun rating(rating: Int) = apply { this.rating = rating }
+        fun respect(respect: Int) = apply { this.respect = respect }
+        fun lastVisit(lastVisit: Date) = apply { this.lastVisit = lastVisit }
+        fun isOnline(isOnline: Boolean) = apply { this.isOnline = isOnline }
+        fun build() = User(
+            id = User.getLastId().toString(),
+            firstName = firstName,
+            lastName = lastName,
+            avatar = avatar,
+            rating = rating,
+            respect = respect,
+            lastVisit = lastVisit,
+            isOnline = isOnline
+        )
+    }
+
     companion object Factory {
         private var lastId: Int = -1
+        fun getLastId() = lastId++
         fun makeUser(fullName: String?): User {
             lastId++
             val (firstName, lastName) = Utils.parseFullName(fullName)
